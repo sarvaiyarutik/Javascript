de8521b79c7f16630031c054d50f7ee75e0ce0a9
Sarvaiya Rutik <sarvaiyarutik18@gmail.com>
2026-06-30 10:15:05 +0530

    add code

--- 15_async_await/02_async.js
+++ 15_async_await/02_async.js
@@ -3,89 +3,89 @@
 
 
 
-// function order(){
+function order(){
 
-//     let item = ["samosa","gathiya"]
+    let item = ["samosa","gathiya"]
 
-//     return new Promise((resolve,reject)=>{
-
-
-//         if(item.length === 0){
-
-//             reject("Please add item");
-//         }
-//         else{
-
-//             setTimeout(()=>{
-
-//                 console.log("Order Book");
-//                 resolve(item);
-//             })
-//         }
-//     })
-// }
-
-// function orderReceive(item){
-
-//     return new Promise((resolve,reject)=>{
-
-//         setTimeout(()=>{
+    return new Promise((resolve,reject)=>{
 
 
-//             resolve(item);
+        if(item.length === 0){
 
-//             console.log("Order Confirm");
+            reject("Please add item");
+        }
+        else{
 
-//         },2000)
-//     })
-// }
+            setTimeout(()=>{
 
-// function orderPeeper(item){
+                console.log("Order Book");
+                resolve(item);
+            })
+        }
+    })
+}
 
-//     let multi = true;
+function orderReceive(item){
 
-//     return new Promise((resolve,reject)=>{
+    return new Promise((resolve,reject)=>{
 
-//         if(!multi){
+        setTimeout(()=>{
 
-//             reject("item not available");
 
-//             console.log("failed this item");
-//         }
+            resolve(item);
 
-//         else{
+            console.log("Order Confirm");
 
-//             console.log("Food Prepared")
+        },2000)
+    })
+}
 
-//             resolve(item);
-//         }
-//     })
-// }
+function orderPeeper(item){
 
-// function foodDelivered(item){
+    let multi = true;
 
-//     return new Promise((resolve)=>{
+    return new Promise((resolve,reject)=>{
 
-//         if(item){
-//             resolve("food delivered");
-//             console.log("enjoy your food");
-//         }
-//     })
-// }
+        if(!multi){
 
-// async function foodOrder(item) {
+            reject("item not available");
+
+            console.log("failed this item");
+        }
+
+        else{
+
+            console.log("Food Prepared")
+
+            resolve(item);
+        }
+    })
+}
+
+function foodDelivered(item){
+
+    return new Promise((resolve)=>{
+
+        if(item){
+            resolve("food delivered");
+            console.log("enjoy your food");
+        }
+    })
+}
+
+async function foodOrder(item) {
     
-//     const f = await order();
+    const f = await order();
     
-//     const o = await orderReceive(f);
+    const o = await orderReceive(f);
 
-//     const p = await orderPeeper(o);
+    const p = await orderPeeper(o);
 
-//     const d = await foodDelivered(p);
+    const d = await foodDelivered(p);
 
-// }
+}
 
-// foodOrder();
+foodOrder();
 
 
 
@@ -179,86 +179,190 @@
     // example  WhatsApp
 
 
-    function TypeSmg(){
+    // function TypeSmg(){
 
-        return new Promise((resolve,reject)=>{
+    //     return new Promise((resolve,reject)=>{
 
-            console.log("typing massage ....")
+    //         console.log("typing massage ....")
 
-            setTimeout(()=>{
-                resolve("Hello Sir");
+    //         setTimeout(()=>{
+    //             resolve("Hello Sir");
 
-            },1000)
-        })
-    }
+    //         },1000)
+    //     })
+    // }
 
-    function sendSmg(Smg){
+    // function sendSmg(Smg){
 
-        return new Promise((resolve,reject)=>{
+    //     return new Promise((resolve,reject)=>{
 
-            console.log("Sending Smg",Smg);
+    //         console.log("Sending Smg",Smg);
 
-            setTimeout(()=>{
-                resolve(Smg);
-            },1500)
-        })
-    }
+    //         setTimeout(()=>{
+    //             resolve(Smg);
+    //         },1500)
+    //     })
+    // }
 
-    function  serverReceive(Smg){
+    // function  serverReceive(Smg){
 
-        return new Promise((resolve,reject)=>{
+    //     return new Promise((resolve,reject)=>{
 
-            console.log("Server Received Message")
+    //         console.log("Server Received Message")
         
-            setTimeout(()=>{
+    //         setTimeout(()=>{
 
-                resolve(Smg);
-            },1500)
-        })
+    //             resolve(Smg);
+    //         },1500)
+    //     })
 
-    }
-
-
-    function friendReceive(Smg){
-
-        return new Promise((resolve,reject)=>{
-
-            console.log("friend received",Smg);
+    // }
 
 
-            setTimeout(()=>{
-                resolve(Smg);
-            },2000)
-        })
-    }
+    // function friendReceive(Smg){
 
-    function seenSmg(Smg){
+    //     return new Promise((resolve,reject)=>{
 
-        return new Promise((resolve,reject)=>{
-
-            console.log("Friend is Reading...");
-
-            setTimeout(()=>{
-                resolve("Seen");
-            },1000)
-        })
-    }
+    //         console.log("friend received",Smg);
 
 
-    async function WhatsAppMessage() {
+    //         setTimeout(()=>{
+    //             resolve(Smg);
+    //         },2000)
+    //     })
+    // }
 
-        let A1 = await TypeSmg();
+    // function seenSmg(Smg){
 
-        let A2 = await sendSmg(A1);
+    //     return new Promise((resolve,reject)=>{
 
-        let A3 = await serverReceive(A2);
+    //         console.log("Friend is Reading...");
 
-        let A4 = await friendReceive(A3);
+    //         setTimeout(()=>{
+    //             resolve("Seen");
+    //         },1000)
+    //     })
+    // }
 
-        let A5 = await seenSmg(A4);
 
-        console.log(A5);
+    // async function WhatsAppMessage() {
+
+    //     let A1 = await TypeSmg();
+
+    //     let A2 = await sendSmg(A1);
+
+    //     let A3 = await serverReceive(A2);
+
+    //     let A4 = await friendReceive(A3);
+
+    //     let A5 = await seenSmg(A4);
+
+    //     console.log(A5);
         
-    }
+    // }
 
-    WhatsAppMessage();
+    // WhatsAppMessage();
+
+
+
+    // example Gaming
+
+
+    // function Login(){
+
+    //     return new Promise((resolve,reject)=>{
+
+    //         console.log("Login....")
+
+    //         setTimeout(()=>{
+
+    //             let LoginSuccess = true;
+
+    //             if(LoginSuccess){
+    //                 resolve("Login Successful");
+    //             }
+    //             else{
+    //                 reject("Login failed")
+    //             }
+
+    //         },2000)
+    //     })
+    // }
+
+    // function matchFound(){
+
+    //     return new Promise((resolve,reject)=>{
+
+    //         console.log("Finding Match....")
+
+    //         setTimeout(()=>{
+
+    //             resolve("Match Found");
+    //         },2000)
+    //     })
+    // }
+
+    // function GameStart(){
+
+    //     return new Promise((resolve,reject)=>{
+
+    //                 console.log("Starting Game...");
+
+
+    //         setTimeout(()=>{
+
+    //             resolve("Game Started")
+    //         },2000)
+    //     })
+    // }
+
+    // function GameEnd(){
+
+    //     return new Promise((resolve,reject)=>{
+
+    //                 console.log("Game Playing...");
+
+
+    //         setTimeout(()=>{
+
+    //             resolve("Game Finished")
+    //         },3000)
+    //     })
+    // }
+
+    // function rewards(){
+
+
+    //     return new Promise((resolve,reject)=>{
+
+    //        console.log("Collecting Rewards...");
+
+
+    //        setTimeout(()=>{
+
+    //         resolve("Rewards Collected")
+    //        },2000)
+
+
+    //     })
+    // }
+
+
+    // async function GamingDetail(){
+
+    //     let A1 = await Login();
+    //     let A2 = await matchFound(A1);
+    //     let A3 = await GameStart(A2);
+    //     let A4 = await GameEnd(A3);
+    //     let A5 = await rewards(A4);
+
+    //     console.log(A5);
+
+    // }
+
+    // GamingDetail();
+
+
+
+
+
