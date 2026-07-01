319f446e4555454fed05ac0c8d58422aec4906aa
Sarvaiya Rutik <sarvaiyarutik18@gmail.com>
2026-07-01 09:42:55 +0530

    added code

--- 15_async_await/04_fatch_api.js
+++ 15_async_await/04_fatch_api.js
@@ -22,3 +22,39 @@ getValue.then((res)=>{
 }).catch((err)=>{
     console.log(err);
 })
+
+
+
+// example
+
+
+
+async function FetchApyKey(){
+
+    const api1 = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+    const api2 = await api1.json();
+
+    console.log(api2);
+}
+
+FetchApyKey();
+
+
+
+
+
+
+const expression = fetch("https://jsonplaceholder.typicode.com/todos");
+
+expression.then((num)=>{
+
+    return num.json();
+}).then((num)=>{
+
+    console.log(num);
+
+}).catch((err)=>{
+
+    console.log(err);
+})
